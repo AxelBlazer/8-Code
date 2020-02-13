38ba8671ce78b395c0643b5dc433b6a06398b705
Axel <58721172+AxelBlazer@users.noreply.github.com>
2020-02-14 01:26:59 +0530

    Add files via upload

---
+++ Java/Practical_13.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+class Practical_13
+{
+	public static void main(String args[])
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter Any Equation");
+		String a = in.nextLine();
+		a = a.replaceAll(" ","");
+		String n1="", n2="", op="";
+
+		for(int i = 0; i < a.length(); i++)
+		{
+			char ch = a.charAt(i);
+			if(op.equals(""))
+			{
+				if((ch >= '0' && ch <= '9') || ch == '.')
+					n1 += ch;
+				else
+					op += ch;
+			}
+			else
+				n2 += ch;
+		}
+		System.out.println("\nSeperate Equation");
+		System.out.println(n1+" ");
+		System.out.println(op+" ");
+		System.out.println(n2+" ");
+
+		double no1 = Double.parseDouble(n1);
+		double no2 = Double.parseDouble(n2);
+		double result=0;
+
+		if(op.equals("+"))
+		{
+			result = no1 + no2;
+			System.out.println("\nResult Is");
+			System.out.println(no1 + op + no2 + "=" + result);
+		}
+		if(op.equals("-"))
+		{
+			result = no1 - no2;
+			System.out.println("\nResult Is");
+			System.out.println(no1 + op + no2 + "=" + result);
+		}
+		if(op.equals("*"))
+		{
+			result = no1 * no2;
+			System.out.println("\nResult Is");
+			System.out.println(no1 + op + no2 + "=" + result);
+		}
+		if(op.equals("/"))
+		{
+			result = no1 / no2;
+			System.out.println("\nResult Is");
+			System.out.println(no1 + op + no2 + "=" + result);
+		}
+	}
+}
+
+
+
+
+
+		
---
+++ Java/Practical_14.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Loan
+{
+	double Amount,Interest,Duration,Total;
+	Loan()
+	{
+		Amount = 0;
+		Interest = 0;
+		Duration = 0;
+		Total = 0;
+	}
+}
+class Circle
+{
+	double radius;
+	Circle()
+	{
+		radius = 0;
+	}
+}
+
+class Practical_14
+{
+	public static void main(String args[])
+	{
+		Loan L = new Loan();
+		Circle C = new Circle();
+		String Str = new String("SSEC");
+		Date D = new Date();
+
+		ArrayList arrlist = new ArrayList(4);
+
+		arrlist.add(L);
+		arrlist.add(C);
+		arrlist.add(Str);
+		arrlist.add(D);
+
+		for(int i = 0; i < arrlist.size(); i++)
+			System.out.println(arrlist.get(i));
+	}
+}
+
+
+	
+	
