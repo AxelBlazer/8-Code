34e2ba17f1e4242f919677bc27a38f9a70121215
Axel <58721172+AxelBlazer@users.noreply.github.com>
2020-02-19 12:16:49 +0530

    Create Practical_07.sh

---
+++ Linux/Practical_07.sh
@@ -0,0 +1,40 @@
+echo "Enter Any Number"
+read n
+echo "Enter Array Number"
+for((i=0;i<n;i++))
+do
+        read no[$i]
+done
+
+for((i=0;i<n;i++))
+do
+        for((j=$i;j<n;j++))
+        do
+                if [ ${no[i]} -lt ${no[j]} ]
+
+                        t=${no[i]}
+                        no[i]=${no[$i]}                                 no[i]=$t
+                fi
+        done
+done
+
+for((i=0;i<n;i++))
+do
+        echo "${no[$i]}"
+done
+
+#Output
+Enter Any Number
+5
+Enter Array Number
+1
+2
+3
+4
+5
+
+5
+4
+3
+2
+1
