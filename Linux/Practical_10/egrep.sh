6069dd59a8e9625b16ab95ed598f1d3d241285ed
Axel <58721172+AxelBlazer@users.noreply.github.com>
2020-04-18 10:24:45 +0530

    Create egrep.sh

---
+++ Linux/Practical_10/egrep.sh
@@ -0,0 +1,39 @@
+echo "1)"
+egrep -c shell 123.txt
+
+echo "2)"
+egrep -L shell 123.txt
+
+echo "3)"
+egrep -v shell 123.txt
+
+echo "4)"
+egrep -i shell 123.txt
+
+echo "5)"
+egrep -n shell 123.txt
+
+echo "6)"
+egrep -m 2 shell 123.tx
+
+#Output
+1)
+3
+2)
+3)
+Shell programming using filters (including grep, egrep, fgrep)
+Study of Unix Shell and Environment Variables.
+4)
+Write a shell script to read n numbers as command arguments and sort them in descending order.
+Write a shell script to display all executable files, directories and zero sized files from current directory.
+Write a shell script to check entered string is palindrome or not.
+Shell programming using filters (including grep, egrep, fgrep)
+Study of Unix Shell and Environment Variables.
+5)
+1:Write a shell script to read n numbers as command arguments and sort them in descending order.
+2:Write a shell script to display all executable files, directories and zero sized files from current directory.
+3:Write a shell script to check entered string is palindrome or not.
+6)
+Write a shell script to read n numbers as command arguments and sort them in descending order.
+Write a shell script to display all executable files, directories and zero sized files from current directory.
+#
