99799664fb5303b61eb3c1bf63f9185af2b3f5ce
Axel <58721172+AxelBlazer@users.noreply.github.com>
2020-04-18 10:26:00 +0530

    Create fgrep.sh

---
+++ Linux/Practical_10/fgrep.sh
@@ -0,0 +1,38 @@
+echo "1)"
+fgrep -c shell 123.txt
+
+echo "2)"
+fgrep -h shell 123.txt
+
+echo "3)"
+fgrep -l shell 123.txt
+
+echo "4)"
+fgrep -v shell 123.txt
+
+echo "5)"
+fgrep -i shell 123.txt
+
+echo "6)"
+fgrep -x shell 123.txt
+
+#Output
+1)
+3
+2)
+Write a shell script to read n numbers as command arguments and sort them in descending order.
+Write a shell script to display all executable files, directories and zero sized files from current directory.
+Write a shell script to check entered string is palindrome or not.
+3)
+123.txt
+4)
+Shell programming using filters (including grep, egrep, fgrep)
+Study of Unix Shell and Environment Variables.
+5)
+Write a shell script to read n numbers as command arguments and sort them in descending order.
+Write a shell script to display all executable files, directories and zero sized files from current directory.
+Write a shell script to check entered string is palindrome or not.
+Shell programming using filters (including grep, egrep, fgrep)
+Study of Unix Shell and Environment Variables.
+6)
+#
