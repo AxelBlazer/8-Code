70af0c2f4560f227e1e69004f574aef76481e9b8
Axel <58721172+AxelBlazer@users.noreply.github.com>
2020-04-18 10:23:32 +0530

    Create grep.sh

---
+++ Linux/Practical_10/grep.sh
@@ -0,0 +1,42 @@
+echo "1)"
+grep -c shell 123.txt
+
+echo "2)"
+grep -l shell 123.txt
+
+echo "3)"
+grep -i shell 123.txt
+
+echo "4)"
+grep -w shell 123.txt
+
+echo "5)"
+grep -o shell 123.txt
+
+echo "6)"
+grep -n shell 123.txt
+
+#Output
+1)
+3
+2)
+123.txt
+3)
+Write a shell script to read n numbers as command arguments and sort them in descending order.
+Write a shell script to display all executable files, directories and zero sized files from current directory.
+Write a shell script to check entered string is palindrome or not.
+Shell programming using filters (including grep, egrep, fgrep)
+Study of Unix Shell and Environment Variables.
+4)
+Write a shell script to read n numbers as command arguments and sort them in descending order.
+Write a shell script to display all executable files, directories and zero sized files from current directory.
+Write a shell script to check entered string is palindrome or not.
+5)
+shell
+shell
+shell
+6)
+1:Write a shell script to read n numbers as command arguments and sort them in descending order.
+2:Write a shell script to display all executable files, directories and zero sized files from current directory.
+3:Write a shell script to check entered string is palindrome or not.
+#
